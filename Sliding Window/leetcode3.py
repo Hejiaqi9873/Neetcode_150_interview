9b4535d11bfd126baff0390451901c65cdfe95d5
Hejiaqi9873 <z5272311@ad.unsw.edu.au>
2023-10-14 20:19:31 +1100

    update

--- Sliding Window/leetcode3.py
+++ Sliding Window/leetcode3.py
@@ -1,1 +1,30 @@
 # 3. Longest Substring Without Repeating Characters
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == '':
+            return 0
+        if len(s) == 1:
+            return 1
+        right = 1
+        queue_item = [s[0]]
+        max_len = 1
+        while right < len(s):
+            if s[right] not in queue_item:
+                queue_item.append(s[right])
+                right += 1
+                if len(queue_item) > max_len:
+                    max_len = len(queue_item)
+            else:
+                if len(queue_item) > max_len:
+                    max_len = len(queue_item)
+                index = queue_item.index(s[right])
+                queue_item = queue_item[index+1:]
+                queue_item.append(s[right])
+                right += 1
+
+
+        return max_len
+
+if __name__ == "__main__":
+    solution = Solution().lengthOfLongestSubstring(s="pwwkew")
+    print(solution)
---
+++ Sliding Window/leetcode424.py
@@ -0,0 +1,34 @@
+# 424. Longest Repeating Character Replacement
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left, right = 0, 0
+        count = defaultdict(int)
+        max_length = 0
+        window_len = 1
+        prev_right = -1
+        while left < len(s) and right < len(s):
+            if right != prev_right:
+                cur = s[right]
+                count[cur] += 1
+
+            # find the max value in count
+            max_value = max(count.values())
+            if window_len - max_value <= k:
+                if window_len > max_length:
+                    max_length = window_len
+                prev_right = right
+                right += 1
+                window_len += 1
+            else:
+                cur_item = s[left]
+                count[cur_item] -= 1
+                left += 1
+                window_len -= 1
+                prev_right = right
+        return max_length
+
+if __name__ == '__main__':
+    solution = Solution().characterReplacement(s = "ABABBAAA", k = 2)
+    print(solution)
